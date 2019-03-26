a5e83b554fb36025c901b3247110992bf886c57e
Katsiaryna Kananchuk <katekononchyk@gmail.com>
2019-03-26 22:04:21 +0300

    add second option function

--- quick sort/index.js
+++ quick sort/index.js
@@ -1,15 +1,17 @@
+// O(n logn)
 function quickSort(array) {
     if (array.length < 2) {
         return array;
     }
     let pivot = array[0];
 
-    let less = array.filter( elem => elem < pivot);
-    let greater = array.filter( elem => elem > pivot);
+    let less = array.filter(elem => elem < pivot);
+    let greater = array.filter(elem => elem > pivot);
+  console.log(less, pivot, greater)
 
-
-return quickSort(less) + pivot + quickSort(greater);
+return quickSort(less).concat(pivot, quickSort(greater));
 }
 
-
-quickSort([4,3,6,1,0])
+var array = [4,3,6,1,0];
+var newArray = quickSort(array);
+console.log(newArray);
